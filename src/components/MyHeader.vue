f89310defd6f0da9d83cc06982ea8bacf39d3e0f
Frowingg <edbico0@gmail.com>
2022-07-15 11:53:48 +0200

    base

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+
+    <div class="header-top">
+        <div>We have a 95% Successful Pass Rate!</div>
+        <div><i class="fa-solid fa-phone-flip"></i>Give us a call to book your tuition! 1-800-555-555</div>
+    </div>
+
+    <div class="header-bottom"></div>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
