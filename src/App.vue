88bb48941479ae4781a94447449cd8b2815fefe0
Frowingg <edbico0@gmail.com>
2022-07-15 12:28:51 +0200

    My header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <!-- <MyHeader /> -->
+    <MyHeader />
   </div>
 </template>
 
 <script>
-// import MyHeader from './components/MyHeader.vue';
+import MyHeader from './components/MyHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    // MyHeader
+    MyHeader
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 .container{
-  width: 70%;
+  width: 60%;
   margin: 0 auto;
+}
+.btn{
+  background-color: #97b27b;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 30px;
+  font-weight: 900;
+  font-size: 11px;
 }
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,11 +2,37 @@
   <header>
 
     <div class="header-top">
-        <div>We have a 95% Successful Pass Rate!</div>
-        <div><i class="fa-solid fa-phone-flip"></i>Give us a call to book your tuition! 1-800-555-555</div>
+        <div class="container">
+            <div>We have a 95% Successful Pass Rate!</div>
+            <div><i class="fa-solid fa-phone-flip"></i>Give us a call to book your tuition! 1-800-555-555</div>
+        </div>
     </div>
 
-    <div class="header-bottom"></div>
+    <div class="header-bottom">
+      <div class="container">
+
+          <img src="../assets/img/avada-drivers-logo-2x-200x39.png" alt="">
+
+          <div class="left-header-bottom">
+              <nav>
+
+                <ul>
+                  <li>HOME</li>
+                  <li>ABOUT</li>
+                  <li>PRICES</li>
+                  <li>COURSES</li> <span class="tag">NEW</span>
+                  <li>LOCATIONS</li>
+                  <li>BLOG</li>
+                </ul>
+
+              </nav>
+
+              <button class="btn">
+                BOOK NOW
+              </button>
+          </div>
+      </div>
+    </div>
 
   </header>
 </template>
@@ -22,5 +48,57 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+.header-top{
+  display: flex;
+  background-color: #484848;
+  color: #d6c9b0;
+  height: 30px;
+  font-size: 9px;
+
+  i{
+    margin-right: 10px;
+  }
+}
+
+.header-bottom{
+    display: flex;
+    height: 90px;
+    background-color: rgba(0,0,0,0.2);
+
+    .left-header-bottom{
+        display: flex;
+
+        ul{
+            list-style-type: none;
+            display: flex;
+
+            li{
+                margin-right: 20px;
+                color: white;
+                font-size: 11px;
+                font-weight: 900;
+                height: 30px;
+                display: flex;
+                align-items: center;
+            }
+            li:hover{
+                color: #97b27b;
+                border-bottom: 3px solid #97b27b;
+            }
+          }
+
+        .tag{
+          font-size: 7px;
+          font-weight: 900;
+          color: white;
+          background-color: #97b27b;
+          height: min-content;
+          padding: 4px;
+          position: relative;
+          right: 15px;
+          top:7px;
+        }
+    }
 }
 </style>
